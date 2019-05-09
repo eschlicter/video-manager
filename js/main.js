049f5f22f57977405fc54ad1909483769b90d839
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-05-09 17:25:01 -0500

    implemented search functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 var data = [];
 
-fetch('https://5cd4377db231210014e3d67b.mockapi.io/api/video')
-.then(function(response) {
+fetch("https://5cd4377db231210014e3d67b.mockapi.io/api/video")
+  .then(function(response) {
     return response.json();
-})
-.then(function(myJson) {
+  })
+  .then(function(myJson) {
     data = myJson;
     data.targetId = "#gridMain";
     updateUI([createVideoCard], [data]);
-});
-        
+
+    var searchBar = document.querySelector("#searchVideos");
+    var videos = document.querySelectorAll(".video-card");
+    var videoData = document.querySelectorAll(".video-name");
+    var searchValue;
+
+    searchBar.addEventListener("input", function(event) {
+      console.log(searchValue);
+      searchValue = event.target.value.toLowerCase();
+      for (let i = 0; i < videos.length; i++) {
+        if (
+          !searchValue ||
+          videoData[i].textContent.toLowerCase().indexOf(searchValue) > -1
+        ) {
+          videos[i].style["display"] = "block";
+        } else {
+          videos[i].style["display"] = "none";
+        }
+      }
+    });
+  });
